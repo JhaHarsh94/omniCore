66eb1e2d001dc82b147d243a07fcca898e247b70
Abhishek Parihar <pariharrajat7@gmail.com>
2026-04-17 23:54:43 +0530

    feat: add login function

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -6,11 +6,12 @@ const jwt = require('jsonwebtoken')
 const userRegistration =  async() =>{
     try{
 
-        const {name,email, password} = req.body
+        const {name,email, password} = req.body // first get the name, email and password...
         
         
-        const hashPassword =  await bcrypt.hash(password,10)
+        const hashPassword =  await bcrypt.hash(password,10) // now hash that password...
         
+        // create the new user and change the password with hashpassword
         const newUser = await User.create({
             name,
             email,
@@ -18,6 +19,7 @@ const userRegistration =  async() =>{
         })
         
         await newUser.save()
+        
         
         res.status(201).json({
             success: true,
@@ -31,4 +33,50 @@ const userRegistration =  async() =>{
         })
     }
 
+}
+
+
+const login = () =>{
+    const {email,password} = req.body // first get the email and password
+    try{
+        const checkTheUser = User.findOne({email}) // now check the user has registered with the same email or not...
+
+
+        // if the user did not register
+        if(!checkTheUser){
+            return  res.json({
+                success: false,
+                message: 'User is not registered...Please register yourself first'
+            })
+        }
+
+        // now compare the password with the password you entered
+        const matchThePassword = bcrypt.compare(password,checkTheUser.password)
+
+        // if password did not match 
+        if(!matchThePassword){
+            return res.json({
+                success: false,
+                message: 'Invalid Pasword...'
+            })
+        }
+
+
+        // now create the token using jwt
+        const token = jwt.sign({
+            id: checkTheUser?.id,
+            role: checkTheUser?.role,
+            username: checkTheUser?.username,
+            password: checkTheUser?.password
+        },
+        process.env.Secret_key,
+        {expiresIn: "60m"}
+    )
+    }
+    catch(err){
+        res.status(500).json({
+            success: false,
+            message: 'Got some error'
+        })
+    }
 }
