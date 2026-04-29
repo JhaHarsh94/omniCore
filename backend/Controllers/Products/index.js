a5854c7d3df773696eaf6e4f5f5dcfba89d158d4
Abhishek Parihar <pariharrajat7@gmail.com>
2026-04-29 23:32:35 +0530

    feat: add CRUD functionality  to product controller

--- backend/Controllers/Products/index.js
+++ backend/Controllers/Products/index.js
@@ -77,7 +77,21 @@ const updateTheProducts = async(req,res) =>{
             })
         }
 
-        
+
+       findProductAndUpdate.title = title || findProductAndUpdate.title
+       findProductAndUpdate.image = image || findProductAndUpdate.image
+       findProductAndUpdate.description = description || findProductAndUpdate.description
+       findProductAndUpdate.category = category || findProductAndUpdate.category
+       findProductAndUpdate.price = price || findProductAndUpdate.price
+       findProductAndUpdate.salePrice = salePrice || findProductAndUpdate.salePrice
+       findProductAndUpdate.totalStock = totalStock || findProductAndUpdate.totalStock
+
+       findProductAndUpdate.save()
+
+       res.status(200).json({
+        success: true,
+        data: findProductAndUpdate
+       })        
 
     }
     catch(err){
@@ -87,3 +101,34 @@ const updateTheProducts = async(req,res) =>{
         })
     }
 }
+
+
+const deleteTheProducts = async(req,res) => {
+    try{
+        const {id} = req.params
+        const deleteProducts = await products.findByIdAndDelete(id)
+
+        if(!deleteProducts){
+            res.status(400).json({
+                success: false,
+                message: 'Product not found'
+            })
+        }
+
+        res.status(200).json({
+            success: true, 
+            message: 'Product deleted successfully...'
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            success: false,
+             message: 'got some error...'
+        })
+    }
+}
+
+
+module.exports = {addProduct,getTheProducts,updateTheProducts,deleteTheProducts}
+
+
